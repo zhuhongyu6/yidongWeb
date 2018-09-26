841635c8a82fc88ec03f3cedfe9adb7fb928adff
tiaopixiaomumu <1315211512@qq.com>
2018-09-26 21:34:45 +0800

    task1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,24 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// TODOList
 export class AppComponent {
-  title = 'gitdemo';
+  txt: string;
+  arr: string[] = [];
+  arr1: string[] = [];
+  getValue(e) {
+    if (e.keyCode === 13) {
+      this.arr.push(this.txt);
+      // console.log(this.arr);
+      this.txt = '';
+    }
+  }
+  delete(idx) {
+     this.arr.splice(idx, 1);        /*splice第一个参数：表示从下标为index的位置开始删除，第二个参数：要删除的个数*/
+     this.arr1.splice(idx, 1);
+  }
+  change(idx) {
+    this.arr1.push(this.arr[idx]);
+    this.arr.splice(idx, 1);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ParentComponent } from './components/parent/parent.component';
+import { ChildlistComponent } from './components/childlist/childlist.component';
+import { ChildinputComponent } from './components/childinput/childinput.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentComponent,
+    ChildlistComponent,
+    ChildinputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
