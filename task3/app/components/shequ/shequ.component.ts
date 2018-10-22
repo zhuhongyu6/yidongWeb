8ee92fb3741bfd0e152543bdbafe52b42b73c390
zhuhongyu6 <1315211512@qq.com>
2018-10-22 10:39:59 +0800

    task 3

---
+++ task3/app/components/shequ/shequ.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shequ',
+  templateUrl: './shequ.component.html',
+  styleUrls: ['./shequ.component.css']
+})
+export class ShequComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
